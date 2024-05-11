675d2806ae9818f1a64141cae95955a624e4ea37
CynthiaZuniga <cynthia.zuniga.a@gmail.com>
2024-05-11 19:19:13 -0400

    first commit

---
+++ backend/consultas.js
@@ -0,0 +1,27 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  allowExitOnIdle: true,
+});
+
+const getInfo = async () => {
+  const consulta = "SELECT * FROM posts;";
+  const { rows } = await pool.query(consulta);
+  return rows;
+};
+
+const posting = async (titulo, url, descripcion) => {
+  const id = Math.floor(Math.random() * 9999);
+  const query =
+    "INSERT INTO posts (id, titulo, img, descripcion, likes) VALUES ($1, $2, $3, $4, $5);";
+  const values = [id, titulo, url, descripcion, 0];
+  const { rows } = await pool.query(query, values);
+  return rows;
+};
+
+module.exports = { getInfo, posting };
